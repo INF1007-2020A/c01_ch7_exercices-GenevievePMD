c3b9b6454b2ba7a143fcd134efb77a405db0ce9f
Genevieve Pelletier-Mc Duff <genevieve.pelletier-mc-duff@polymtl.ca>
2020-10-19 16:08:05 -0400

    Exercices 1-3 complétés

--- exercice.py
+++ exercice.py
@@ -3,8 +3,21 @@
 
 # TODO: Importez vos modules ici
 from math import pi
-import turtle
-import ch6
+from turtle import pendown, penup, setpos, back, forward, left, right, color
+    # 3 premiers sont pour le tronc
+    # back = prend curseur et fait trait par en arrière
+    # forward = dessin vers l'avant
+    # left : tounrer curseur vers la gauche avec angle spécifié
+    # right : tourner curseur vers la droite avec angle spécifié
+    # left/right ne dessine pas
+
+
+from ch6 import histogram   # fonction qui retourne un dictionnaire
+
+MAX_DEPTH = 4
+BRANCH_LENGTH = 100
+SHRINK_FACTOR = 0.8
+TURN_ANGLE = 20 # degrees
 
 
 # TODO: Définissez vos fonction ici
@@ -14,12 +27,54 @@ def ellipsoide(a :float= 1, b :float= 1, c :float= 1, masse_volumique :float= 1)
     masse = volume * masse_volumique
     return masse, volume
 
-# Exercice 2 - À Compléter
-# Avec import ch6
+# Exercice 2 - À Compléter - Avec import ch6
+def sort(sequence : str) -> tuple:
+    dict_sequence = {}
+
+    # return sorted(histogram(sequence).items(), reverse = True, key = lambda x:x[1])[0]  # inversé l'ordre les éléments avec le reverse
+    # prendre le premier élément d,une séquence inversée. revient à prendre le dernier élément d'une séquence normale.
+    #équivalent
+    return sorted(histogram(sequence).items(), key = lambda x:x[1])[-1] # histogram est un dictionnaire. pour ça que peut faire .items
+
 
 
 # Exercice 3 - Dessiner un arbre en utilisant la récursivité
 
+def draw_trunk() -> None:
+    color('green')
+    left(90)    # Arbre à 90 degrées, donc perpendiculaire au sol. "debout"
+    penup()
+    setpos(x = 0, y = -200)
+    pendown()
+    forward(BRANCH_LENGTH)
+
+def draw_left(depth : int, distance : float) -> None:
+    left(TURN_ANGLE)
+    forward(distance + SHRINK_FACTOR)
+
+    draw_tree(depth + 1)    # sortir de la fonction draw_tree
+
+    back(distance + SHRINK_FACTOR)  # retourner en arrière de la distance, faire la partie droite
+    right(TURN_ANGLE)
+
+
+def draw_right(depth : int, distance : float) -> None:
+    right(TURN_ANGLE)
+    forward(distance + SHRINK_FACTOR)
+
+    draw_tree(depth + 1)    # sortir de la fonction draw_tree
+
+    back(distance + SHRINK_FACTOR)  # retourner en arrière de la distance, faire la partie gauche
+    left(TURN_ANGLE)
+
+
+
+def draw_tree(depth : int) -> None:
+    distance = BRANCH_LENGTH * SHRINK_FACTOR ** depth   # Distance doit diminuée car dans l'arbre, les branches plus loins, niveau plus élevé, doivent être plus petites
+
+    if depth <= MAX_DEPTH : # Critère d'arrêt. quand rendu au bout de l'arbre, retourne au début
+        draw_left(depth, distance)  # portion de gauche de l'arbre
+        draw_right(depth, distance)
 
 
 
@@ -30,5 +85,14 @@ if __name__ == '__main__':
     masse,volume = ellipsoide(1,5,6,10)
     print(f'Un ellipsoide a une masse de {round(masse,3)} kg et un volume de {round(volume,3)} m^3.')
 
+    # Exercice 2
+    print(sort("nyaaaan Cat"))
 
-    pass
+    # Exercice 3
+    ''' Dessiner le tronc, côté gauche. et ensuite faire le côté droit. Continuer, revenir, continuer, revenir'''
+    draw_trunk()
+    draw_tree(0)    # Fonction récursive
+
+
+
+
